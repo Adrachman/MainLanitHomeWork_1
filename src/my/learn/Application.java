8e9138d11c32479b1b58a70cedc9713326bbe859
Adrachman <adra_admiral@mail.ru>
2021-05-10 17:54:33 +0300

    First

---
+++ src/my/learn/Application.java
@@ -0,0 +1,15 @@
+package my.learn;
+
+public class Application {
+
+    public static void main(String[] args) {
+        Kotik barsik = new Kotik("Barsik",18,15,"May",5);
+        Kotik garfild = new Kotik();
+        garfild.setKotik("garfild",-3,40,"May");
+        barsik.liveAnotherDay();
+        System.out.println(barsik.getName()+ "\t" +barsik.getWeight());
+        System.out.println(barsik.getMeow().equals(garfild.getMeow()));
+        System.out.println(Kotik.catsAmount);
+
+    }
+}
